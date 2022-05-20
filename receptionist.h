84c07397999da06b4fd3be1291ef8348ffba4844
it21328770 <99583285+it21328770@users.noreply.github.com>
2022-05-20 12:16:01 +0000

    receptionist.h

---
+++ receptionist.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Receipt.h"
+#define size 2
+class Receptionist
+{
+private:
+	int staffId;
+	char staffName[20];
+	char staffEmail[40];
+	Receipt* rec[size];
+
+public:
+	Receptionist();
+	Receptionist(int sId, const char sName[], const char sEmail[]);  
+	void displayDetails();
+	~Receptionist();
+
+};
